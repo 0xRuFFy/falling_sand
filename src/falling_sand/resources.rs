d4eff0fcd1aac766e08e1fafcd55a35d94a2a7d1
luffy <opora.privat@gmail.com>
2023-12-13 12:30:58 +0100

    added a new Brush

--- src/falling_sand/resources.rs
+++ src/falling_sand/resources.rs
@@ -9,3 +9,57 @@ impl Default for CurrentParticleType {
         Self(ParticleType::Sand)
     }
 }
+
+type PBrush = &'static [IVec2];
+
+const BRUSH_DOT: PBrush = &[IVec2::new(0, 0)];
+const BRUSH_MEDIUM: PBrush = &[
+    IVec2::new(0, 1),
+    IVec2::new(0, -1),
+    IVec2::new(0, 3),
+    IVec2::new(0, -3),
+    IVec2::new(1, 1),
+    IVec2::new(1, -1),
+    IVec2::new(1, 3),
+    IVec2::new(1, -3),
+    IVec2::new(-1, 1),
+    IVec2::new(-1, -1),
+    IVec2::new(-1, 3),
+    IVec2::new(-1, -3),
+    IVec2::new(2, 0),
+    IVec2::new(2, 2),
+    IVec2::new(2, -2),
+    IVec2::new(-2, 0),
+    IVec2::new(-2, 2),
+    IVec2::new(-2, -2),
+    IVec2::new(3, 0),
+    IVec2::new(3, 2),
+    IVec2::new(3, -2),
+    IVec2::new(-3, 0),
+    IVec2::new(-3, 2),
+    IVec2::new(-3, -2),
+    IVec2::new(4, 1),
+    IVec2::new(4, -1),
+    IVec2::new(-4, 1),
+    IVec2::new(-4, -1),
+];
+
+#[derive(Resource)]
+pub struct ParticleBrush(PBrush);
+
+impl ParticleBrush {
+    // pub fn use_medium(&mut self) {
+    //     self.0 = BRUSH_MEDIUM;
+    // }
+
+    pub fn get(&self) -> PBrush {
+        self.0
+    }
+}
+
+impl Default for ParticleBrush {
+    fn default() -> Self {
+        // Self(BRUSH_DOT)
+        Self(BRUSH_MEDIUM)
+    }
+}
